d964f31c4b0e3517b82f89ad191c88384d03785f
xuandong <xuandong@kaihongdigi.com>
2022-08-20 17:48:20 +0800

    modify LICENSE_2
    Signed-off-by: xuandong <xuandong@kaihongdigi.com>

--- khdvk-3566b/wifi/bcmdhd_hdf/bcmdhd/include/802.11e.h
+++ khdvk-3566b/wifi/bcmdhd_hdf/bcmdhd/include/802.11e.h
@@ -1,7 +1,7 @@
 /*
  * 802.11e protocol header file
  *
- * Copyright (C) 1999-2019, Broadcom.
+ * Copyright (C) 2022 Broadcom.
  *
  *      Unless you and Broadcom execute a separate written software license
  * agreement governing use of this software, this software is licensed to you
