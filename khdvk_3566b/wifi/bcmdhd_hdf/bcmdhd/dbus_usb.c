5d7684639a7f790bd061f4cf32bd2a8d06a08532
fanyongheng <fanyongheng@kaihongdigi.com>
2022-08-29 19:30:45 +0800

    修改WiFi版权问题
    Signed-off-by: fanyongheng <fanyongheng@kaihongdigi.com>

--- khdvk_3566b/wifi/bcmdhd_hdf/bcmdhd/dbus_usb.c
+++ khdvk_3566b/wifi/bcmdhd_hdf/bcmdhd/dbus_usb.c
@@ -1,7 +1,7 @@
 /*
  * Dongle BUS interface for USB, OS independent
  *
- * Copyright (C) 2022 Broadcom Corporation
+ * Copyright (C) 1999-2016, Broadcom Corporation
  * 
  *      Unless you and Broadcom execute a separate written software license
  * agreement governing use of this software, this software is licensed to you
